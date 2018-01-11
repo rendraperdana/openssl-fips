60e9e00025fb98667935984fe79aa39855161856
root <rendraperdana@hotmail.com>
2018-01-11 11:03:33 +0700

    fixing fips removal

--- install-replace.sh
+++ install-replace.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 cd ./openssl-fips-2.0.16
-rm -rf fips
+make clean
 ./config fips no-ec2m no-ssl2 no-ssl3 no-weak-ssl-ciphers -O3 -Ofast -march=native
 make depend -j$(grep -c ^processor /proc/cpuinfo)
 make -j$(grep -c ^processor /proc/cpuinfo)
@@ -10,6 +10,7 @@ make install
 cd ../
 ln -s ./openssl-fips-2.0.16/fips fips
 
+make clean
 ./config fips no-ec2m no-ssl2 no-ssl3 no-weak-ssl-ciphers -O3 -Ofast -march=native --prefix=/usr --openssldir=/usr shared
 make depend -j$(grep -c ^processor /proc/cpuinfo)
 make -j$(grep -c ^processor /proc/cpuinfo)
